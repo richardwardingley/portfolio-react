9e4e1664aaceccf7788bb1907e607d6011b0d1e4
Richard Wardingley <richardwardingley@hotmail.com>
2020-06-29 22:57:43 +0100

    Updates

---
+++ precache-manifest.b30543299aaccbef7d985ff9a4b3fd0c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47f31381da8e57547a8855f45e012ad",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "8195ba3641930b641f15",
+    "url": "/portfolio-react/static/css/main.85d31f57.chunk.css"
+  },
+  {
+    "revision": "5bf763d1c85fa111f907",
+    "url": "/portfolio-react/static/js/2.e60d46ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.e60d46ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8195ba3641930b641f15",
+    "url": "/portfolio-react/static/js/main.7359527a.chunk.js"
+  },
+  {
+    "revision": "105286aac3527f3b7f6e",
+    "url": "/portfolio-react/static/js/runtime-main.78f6c475.js"
+  },
+  {
+    "revision": "71837401e9bf10020708e4716d4585fd",
+    "url": "/portfolio-react/static/media/csslogo.71837401.png"
+  },
+  {
+    "revision": "346961ebdd764f88068de983bc57673d",
+    "url": "/portfolio-react/static/media/githublogo.346961eb.png"
+  },
+  {
+    "revision": "4579fdec6dc96342e17dd0b40b0c0921",
+    "url": "/portfolio-react/static/media/gym.4579fdec.png"
+  },
+  {
+    "revision": "e42fd4cb7a977d58487227f87029c738",
+    "url": "/portfolio-react/static/media/htmllogo.e42fd4cb.png"
+  },
+  {
+    "revision": "b54fc1c9e4f229859a1b00205f5d14c0",
+    "url": "/portfolio-react/static/media/jslogo.b54fc1c9.png"
+  },
+  {
+    "revision": "7247ed4398bb98c44cecd83901b181bd",
+    "url": "/portfolio-react/static/media/mongologo.7247ed43.png"
+  },
+  {
+    "revision": "582bae12ee9ad893ff4caef9bf1bd08a",
+    "url": "/portfolio-react/static/media/nodelogo.582bae12.png"
+  },
+  {
+    "revision": "704fb403858cd96c845b8ff21fcea3f5",
+    "url": "/portfolio-react/static/media/pianouke.704fb403.jpeg"
+  },
+  {
+    "revision": "22eeecf99b8d92726b4f3735be5473c0",
+    "url": "/portfolio-react/static/media/pythonlogo.22eeecf9.png"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/portfolio-react/static/media/reactlogo.70f87bba.png"
+  }
+]);

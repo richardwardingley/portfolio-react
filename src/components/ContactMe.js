c7268f10d4e7a224f974e3e755ba9974013231d0
Richard Wardingley <richardwardingley@hotmail.com>
2020-07-07 22:22:06 +0100

    updated contact me section

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Navbar from './Navbar';
 import './ContactMe.css';
+import {FaLinkedin} from 'react-icons/fa';
+import {BsEnvelopeFill} from 'react-icons/bs';
+import {FaPhoneAlt} from 'react-icons/fa';
 
 const ContactMe = () => {
     return (
         <div>
             <div id="contactTitle">Contact me</div>
             <div id="contactText">
-                For any queries please contact me via email: <a href="mailto:richardwardingley@hotmail.com" target= "_blank" >richardwardingley@hotmail.com</a><br/>
-                or via mobile on 07743 02 02 05
+                If there are any opportunities you would like to discuss, please feel free to contact me.<br/>
+                <div className="contactIcons">
+                <BsEnvelopeFill/> &nbsp; &nbsp;<a href="mailto:richardwardingley@hotmail.com" target= "_blank" className= "contactLinks" >richardwardingley@hotmail.com</a>
+                </div>
+                <div className="contactIcons">
+                <FaPhoneAlt/> &nbsp; &nbsp;07743 02 02 05
+                </div>
+                <div className="contactIcons">
+                <FaLinkedin/> &nbsp; &nbsp;<a href="https://www.linkedin.com/in/richardwardingley/" target="_blank" className= "contactLinks" >LinkedIn</a>
+                </div>
             </div>
             <div id="footer">
                     Richard Wardingley ©2020
